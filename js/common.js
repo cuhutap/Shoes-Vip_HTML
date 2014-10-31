0ded5b1b8b89bee383a864b4b542555c8ff4914d
CuHuTap <sain_195@mail.ru>
2014-10-31 13:49:51 +0200

    Состояние на вечер 30.10

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function () {
+	$('.jcarousel').jcarousel({
+
+	});
+	$('.Jcarousel_next').jcarouselControl({
+		target: '+=1'
+	});
+	$('.Jcarousel_prev').jcarouselControl({
+		target: '-=1'
+	});
+});
+
